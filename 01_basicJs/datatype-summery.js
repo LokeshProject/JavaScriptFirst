545f32b7331531ab70106b33e4869950864decf0
LokeshProject <lokesh.bintu1@gmail.com>
2024-12-13 10:23:30 +0000

    ..

---
+++ 01_basicJs/datatype-summery.js
@@ -0,0 +1,40 @@
+//premitiive 
+// 7 Type :- Number,String,Boolen,Null,Undefine,Bigint,Symbole
+// non premetive or refrence type :- Arreay,Object,Function
+
+//number
+let id=10;
+// string
+let name ='Lokesh'
+//boolean
+let isLogin=true;
+//null
+let temp=null;
+//Undefine
+let rollno;
+let rollno1=undefined;
+//Bigint
+let Bigint=2343223432343343n;
+//Symbole
+let sym1=Symbol('123')
+let sym2=Symbol('123')
+
+console.log(sym1,sym2,sym1===sym2)
+
+//Arreay
+let fruits=['banana','mango','orange']
+//Object
+let color ={
+    red:red,
+    green:Green,
+    yellow:Yellow,
+
+   class: {
+      2:2
+    },
+}
+
+//function
+const bike = function bike(){
+    console.log('Dominar');
+}
