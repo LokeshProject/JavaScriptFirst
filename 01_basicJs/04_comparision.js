9f663cc5459d50ad971faefb3f0043406b567483
LokeshProject <lokesh.bintu1@gmail.com>
2024-12-13 07:27:37 +0000

    hhh

---
+++ 01_basicJs/04_comparision.js
@@ -0,0 +1,16 @@
+console.log(1 == '1');
+console.log(1>'0');
+console.log(null == 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null >= 0);
+console.log(undefined == 0);
+console.log(undefined < 0);
+console.log(undefined <= 0);
+console.log(undefined >= 0);
+
+
+//it check data type also
+console.log(null === 0);
+console.log(undefined === 0);
+
