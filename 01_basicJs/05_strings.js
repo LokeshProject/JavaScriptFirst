f17e30ee1757fd2f7bf910a9d6658fbfe6730d6d
LokeshProject <lokesh.bintu1@gmail.com>
2024-12-31 10:28:25 +0000

    ...

---
+++ 01_basicJs/05_strings.js
@@ -0,0 +1,15 @@
+const myName=' Lokesh '
+const age='23'
+console.log(`Hi My Name is ${myName} and i am ${age} year old`)
+const testName= new String('Lokesh')
+console.log(testName.toUpperCase());
+console.log(testName.charAt(2));
+console.log(testName.indexOf('s'));
+console.log(myName.substring(0,5));
+console.log(myName.slice(-5,6));
+console.log(myName.trim(),myName);
+const url =`https://www.onlinego.in/`
+console.log(url.replace('onlinego','youtube'));
+console.log(url.includes('onlinego'),url.includes('lokesh'));
+console.log(url.split('t'));
+
