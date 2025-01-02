39c30f15e2a927c6af2f6c68aed6bc37ec3a929d
LokeshProject <lokesh.bintu1@gmail.com>
2025-01-02 06:38:51 +0000

    .

---
+++ 01_basicJs/07_dates.js
@@ -0,0 +1,10 @@
+let date = new Date();
+// console.log(date);
+// console.log(date.toLocaleDateString());
+// console.log(date.toJSON());
+// console.log(date.toISOString());
+// console.log(date.getDay());
+
+let mydate= new Date(2025 ,0 ,12)
+console.log(mydate.toDateString());
+
