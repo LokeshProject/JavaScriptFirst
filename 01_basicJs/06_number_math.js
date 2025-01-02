0e79a6be24e845cd2251d2eaaeb8eaada900f72d
LokeshProject <lokesh.bintu1@gmail.com>
2025-01-02 06:13:01 +0000

    ....

---
+++ 01_basicJs/06_number_math.js
@@ -0,0 +1,36 @@
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+const balance2 =123.4567;
+// console.log(balance2.toPrecision(4));
+const sal =10000000;
+// console.log(sal.toLocaleString());
+// console.log(sal.toLocaleString('en-IN'));
+
+//+++++++++Mat
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.abs(+4));
+//it give only value it remove - or + 
+// console.log(Math.round(1.3));
+// console.log(Math.round(1.6));
+// console.log(Math.round(1.5));
+// console.log(Math.ceil(1.2));
+// console.log(Math.floor(1.9));
+
+
+// the value contain in between 0-1
+console.log(Math.random());
+console.log((Math.random()*10) +1);
+
+
+const min=1000;
+const max=9999;
+console.log(Math.floor(Math.random() * (max-min +1))+min);
+
+
+
+
+
+
