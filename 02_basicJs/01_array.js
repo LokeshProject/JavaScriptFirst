bd9afcebda937a803e7d41190844d344fcdf8d55
LokeshProject <lokesh.bintu1@gmail.com>
2025-01-06 10:49:57 +0000

    ..

---
+++ 02_basicJs/01_array.js
@@ -0,0 +1,32 @@
+const arry =[1,2,3,4,5,6]
+
+console.log(arry);
+console.log(arry[1]);
+const arry2 = new Array(1,2,3,4)
+console.log(arry2);
+  
+
+//aad new value in last 
+arry.push(7)
+
+arry.push(8)
+console.log(arry);
+
+//remove value from last
+arry.pop();
+console.log(arry);
+//add new itam in start
+arry.unshift(9)
+console.log(arry);
+//Remove itamfrom start
+arry.shift()
+console.log(arry);
+//it finding it present or not ,it give boolen value output
+console.log(arry.includes(6));
+//it finding it present or not ,it give boolen value output
+console.log(arry.includes(9));
+// when index not present it give -1
+console.log(arry.indexOf(9));
+// when index present then it give that index value
+console.log(arry.indexOf(3));
+
