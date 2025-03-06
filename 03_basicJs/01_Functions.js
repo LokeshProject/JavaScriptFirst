04514951d31e434769bcea2d8b7792ed30e2fc05
LokeshProject <lokesh.bintu1@gmail.com>
2025-03-06 06:46:13 +0000

    ...

---
+++ 03_basicJs/01_Functions.js
@@ -0,0 +1,24 @@
+// function restSpread (...val1){
+// return val1
+// }
+// console.log(restSpread(100,200,300,400))//[ 100, 200, 300, 400 ]
+
+function obj(details){
+console.log(
+    `har name is ${details.name} & age ${details.age}`
+)
+}
+const lok ={
+    name:'Snigdha',
+    age:24
+}
+obj(lok)
+
+
+
+// function arr(arrr){
+//     return arrr//[ 'Niru', 'snigdha', 'lokesh', 'sibani' ]
+//     return arrr[1]//snigdha
+// }
+// const array =['Niru','snigdha','lokesh','sibani']
+// console.log(arr(array))
